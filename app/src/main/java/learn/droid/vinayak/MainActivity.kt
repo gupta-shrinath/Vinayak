3d3c3bbb26120d0f8f70ca0909b91e79ce9f9c56
Gupta Shrinath <guptashrinath9@gmail.com>
2021-09-10 14:14:51 +0530

    Create button and navigate to next activity

--- app/src/main/java/learn/droid/vinayak/MainActivity.kt
+++ app/src/main/java/learn/droid/vinayak/MainActivity.kt
@@ -1,11 +1,19 @@
 package learn.droid.vinayak
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val reportButton = findViewById<Button>(R.id.report_btn)
+        reportButton.setOnClickListener {
+            startActivity(Intent(this,CaptureDisasterActivity::class.java))
+        }
+
     }
 }
