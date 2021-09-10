ac144612f39c0f9fe8f8706fdc3a4fc1bf9f82e7
Gupta Shrinath <guptashrinath9@gmail.com>
2021-09-10 14:15:33 +0530

    Create CaptureDisasterActivity

---
+++ app/src/main/java/learn/droid/vinayak/CaptureDisasterActivity.kt
@@ -0,0 +1,11 @@
+package learn.droid.vinayak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CaptureDisasterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_capture_disaster)
+    }
+}
