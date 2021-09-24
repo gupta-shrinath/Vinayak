3ed2ed902aefe0162681f3fd5deba02aa3eb569a
Gupta Shrinath <guptashrinath9@gmail.com>
2021-09-24 14:15:31 +0530

    Use ReverseGeocoding

--- app/src/main/java/learn/droid/vinayak/receivers/LocationBroadcastReceiver.kt
+++ app/src/main/java/learn/droid/vinayak/receivers/LocationBroadcastReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import learn.droid.vinayak.utils.ReverseGeoCoder
 
 class LocationBroadcastReceiver: BroadcastReceiver() {
     companion object {
@@ -11,10 +12,19 @@ class LocationBroadcastReceiver: BroadcastReceiver() {
     }
 
     override fun onReceive(p0: Context?, p1: Intent?) {
+        var longitude = 0.0
+        var latitude = 0.0
         if (p1 != null) {
-            p1.getStringExtra("longitude")?.let { Log.d(TAG, "Current Longitude $it") }
-            p1.getStringExtra("latitude")?.let { Log.d(TAG, "Current Latitude $it") }
+            p1.getStringExtra("longitude")?.let {
+                Log.d(TAG, "Current Longitude $it")
+                longitude = it.toDouble()
+            }
+            p1.getStringExtra("latitude")?.let {
+                Log.d(TAG, "Current Latitude $it")
+                latitude = it.toDouble()
+            }
         }
+        Log.d(TAG, "onReceive: "+ p0?.let { ReverseGeoCoder.getLocation(it,longitude,latitude) })
     }
 
 
