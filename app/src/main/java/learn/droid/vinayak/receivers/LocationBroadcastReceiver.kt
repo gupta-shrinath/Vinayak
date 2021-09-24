135f60a31e75d9603820904ff55a5a1d195a4729
Gupta Shrinath <guptashrinath9@gmail.com>
2021-09-24 16:45:01 +0530

    Add location details to database

--- app/src/main/java/learn/droid/vinayak/receivers/LocationBroadcastReceiver.kt
+++ app/src/main/java/learn/droid/vinayak/receivers/LocationBroadcastReceiver.kt
@@ -4,9 +4,21 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import learn.droid.vinayak.database.LocationDatabase
+import learn.droid.vinayak.database.entities.Location
 import learn.droid.vinayak.utils.ReverseGeoCoder
+import kotlin.coroutines.CoroutineContext
 
-class LocationBroadcastReceiver: BroadcastReceiver() {
+class LocationBroadcastReceiver() : BroadcastReceiver(), CoroutineScope {
+    private var job: Job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
     companion object {
         private val TAG = LocationBroadcastReceiver::class.simpleName
     }
@@ -25,6 +37,11 @@ class LocationBroadcastReceiver: BroadcastReceiver() {
             }
         }
         Log.d(TAG, "onReceive: "+ p0?.let { ReverseGeoCoder.getLocation(it,longitude,latitude) })
+        val locationDao = p0?.let { LocationDatabase.getInstance(it).locationDao()}
+        val location = Location(longitude = longitude,latitude = latitude)
+        launch{
+            locationDao?.insertLocation(location)
+        }
     }
 
 
